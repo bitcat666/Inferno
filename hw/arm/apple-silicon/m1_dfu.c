e234a279091902896d344017eebe1c0fc80065df
bitcat666 <bitcat666@users.noreply.github.com>
2026-06-14 06:58:54 -0700

    m1_dfu: add zero terminator to Property array

--- hw/arm/apple-silicon/m1_dfu.c
+++ hw/arm/apple-silicon/m1_dfu.c
@@ -430,6 +430,7 @@ static Property m1_dfu_properties[] = {
     DEFINE_PROP_UINT32("cpid", M1DFUState, cpid, 0x8103),
     DEFINE_PROP_UINT32("bdid", M1DFUState, bdid, 0x22),
     DEFINE_PROP_UINT64("ecid", M1DFUState, ecid, 0x00000001A2B3C4D5ULL),
+    { /* end of list */ },
 };
 
 /* ── VMState ────────────────────────────────────────────── */
